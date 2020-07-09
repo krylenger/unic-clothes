c0811f78bd4ecf92c9967bb11bf25530680ef3fc
krylenger <krylenger@gmail.com>
2020-07-09 12:13:39 +0800

    breaking everything into smaller components, styling menu-container

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './homepage.styles.scss';
+import MenuContainer from '../../components/menu-container/menu-container.component';
+
+const HomePage = () => (
+  <div className="homepage">
+    <MenuContainer />
+  </div>
+);
+
+export default HomePage;
