311b2085eca4ae5c020374a24ee91579c029d267
krylenger <krylenger@gmail.com>
2020-07-10 18:27:49 +0800

    adding collection-item component and styling it

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from '../collection-item/collection-item.component'
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
@@ -7,8 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map(({ id, name }) => (
-          <div key={id}>{name}</div>
+        .map(({ id, ...otherItemParams }) => (
+          <CollectionItem key={id} {...otherItemParams}/>
         ))}
     </div>
   </div>
